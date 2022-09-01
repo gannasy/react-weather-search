902277e341654be21ece509819d69e10d33b5bfc
jellyfish28 <gkonuhovam@gmail.com>
2022-09-01 14:45:06 +0300

    footer added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello </h1>
+      Weather App
+
+      <footer>
+        This app was coded by me as
+        <a href="https://github.com/gannasy/react-weather-search" target="_blank"> open-sourced</a>
+      </footer>
     </div>
   );
 }
